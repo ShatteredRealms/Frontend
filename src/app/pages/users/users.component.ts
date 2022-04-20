0d752967e5bac13d1e844e07efd0bae6745a0aa4
Wil Simpson <simpson.wil255@gmail.com>
2022-04-20 15:25:48 -0500

    Added profile page

---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/user.model";
+import {Permission} from "../../models/permission.model";
+import {Role} from "../../models/role.model";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+  user: User;
+
+  constructor(protected route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.user = {
+      email: "user@example.com",
+      first_name: "Furstlee",
+      id: 9001,
+      last_name: "Lastinson",
+      permissions: [Permission.TEST_PERMISSION],
+      role: Role.USER,
+      token: "",
+      username: "username"
+    };
+
+    this.route.params.subscribe((params) => {
+      this.user.id = params['user'];
+    });
+  }
+}
