ea0dac826ea34c81721fac0a8ffeade6aafcd341
Wil Simpson <simpson.wil255@gmail.com>
2022-04-20 14:17:58 -0500

    Remove login button when signed in

--- src/app/_components/footer/footer.component.ts
+++ src/app/_components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../_services/authentication.service";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+
+  isUserSignedIn(): boolean {
+    return this.authService.currentUserValue != undefined;
+  }
 }
