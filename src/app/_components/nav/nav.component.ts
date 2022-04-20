7b6d1284030b1c6215cc72cf22a7eab122729a36
Wil Simpson <simpson.wil255@gmail.com>
2022-04-20 17:22:28 -0500

    Account button now changes from dropdown to buttons with nav

--- src/app/_components/nav/nav.component.ts
+++ src/app/_components/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthenticationService} from "../../_services/authentication.service";
+import {fromEvent} from "rxjs";
+import {debounceTime, map, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-nav',
@@ -13,12 +15,24 @@ export class NavComponent implements OnInit {
     {title: 'Login', link: '/login'},
   ];
 
+  isScreenSmall$: any;
+
   constructor(protected route: ActivatedRoute,
               protected router: Router,
               public authService: AuthenticationService) { }
 
   ngOnInit(): void {
     console.log('user:', this.authService.currentUserValue);
+
+    // Checks if screen size is less than 1024 pixels
+    const checkScreenSize = () => document.body.offsetWidth < 992;
+
+    // Create observable from window resize event throttled so only fires every 500ms
+    const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(200), map(checkScreenSize));
+
+    // Start off with the initial value use the isScreenSmall$ | async in the
+    // view to get both the original value and the new value after resize.
+    this.isScreenSmall$ = screenSizeChanged$.pipe(startWith(checkScreenSize()))
   }
 
   isUserSignedIn(): boolean {
